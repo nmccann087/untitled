7726625025689dabb625150954a9b7df27bfd9e5
nmccann087 <74011070+nmccann087@users.noreply.github.com>
2020-11-23 10:19:18 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+let number = 0
+input.onGesture(Gesture.Shake, function () {
+    number = randint(1, 6)
+    basic.showNumber(number)
+    basic.pause(3500)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (number == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+    for (let index = 0; index < number; index++) {
+        music.playMelody("F F F - - - - - ", 290)
+    }
+})
